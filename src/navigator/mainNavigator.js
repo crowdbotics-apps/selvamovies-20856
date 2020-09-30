504d891785d09ea39cf125f539f93ac9e8f1c52f
Crowdbotics <team@crowdbotics.com>
2020-09-30 00:15:35 +0000

    remove package blueprint-react-native-settings-screen

--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -14,7 +14,6 @@ import ArticleList117143Navigator from '../features/ArticleList117143/navigator'
 import ArticleList117126Navigator from '../features/ArticleList117126/navigator';
 import ArticleList117125Navigator from '../features/ArticleList117125/navigator';
 import ArticleList117124Navigator from '../features/ArticleList117124/navigator';
-import Settings2117122Navigator from '../features/Settings2117122/navigator';
 import CalendarView3117121Navigator from '../features/CalendarView3117121/navigator';
 
 /**
@@ -33,7 +32,6 @@ ArticleList117143: { screen: ArticleList117143Navigator },
 ArticleList117126: { screen: ArticleList117126Navigator },
 ArticleList117125: { screen: ArticleList117125Navigator },
 ArticleList117124: { screen: ArticleList117124Navigator },
-Settings2117122: { screen: Settings2117122Navigator },
 CalendarView3117121: { screen: CalendarView3117121Navigator },
 
     /** new navigators can be added here */
