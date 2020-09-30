5b9c42f169388c86f0325b204e686d0d7ddacec0
Crowdbotics <team@crowdbotics.com>
2020-09-30 00:15:31 +0000

    remove package blueprint-react-native-sign-up-1-screen

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -9,7 +9,6 @@ export const installed_blueprints = [
 { name: 'ArticleList117126', human_name: 'Article List', access_route: 'ArticleList117126'},
 { name: 'ArticleList117125', human_name: 'Article List', access_route: 'ArticleList117125'},
 { name: 'ArticleList117124', human_name: 'Article List', access_route: 'ArticleList117124'},
-{ name: 'SignUp11117123', human_name: 'Sign Up 1 1', access_route: 'SignUp11117123'},
 { name: 'Settings2117122', human_name: 'Settings 2', access_route: 'Settings2117122'},
 { name: 'CalendarView3117121', human_name: 'Calendar View 3', access_route: 'CalendarView3117121', icon: 'calendar'},
 
--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -14,7 +14,6 @@ import ArticleList117143Navigator from '../features/ArticleList117143/navigator'
 import ArticleList117126Navigator from '../features/ArticleList117126/navigator';
 import ArticleList117125Navigator from '../features/ArticleList117125/navigator';
 import ArticleList117124Navigator from '../features/ArticleList117124/navigator';
-import SignUp11117123Navigator from '../features/SignUp11117123/navigator';
 import Settings2117122Navigator from '../features/Settings2117122/navigator';
 import CalendarView3117121Navigator from '../features/CalendarView3117121/navigator';
 
@@ -34,7 +33,6 @@ ArticleList117143: { screen: ArticleList117143Navigator },
 ArticleList117126: { screen: ArticleList117126Navigator },
 ArticleList117125: { screen: ArticleList117125Navigator },
 ArticleList117124: { screen: ArticleList117124Navigator },
-SignUp11117123: { screen: SignUp11117123Navigator },
 Settings2117122: { screen: Settings2117122Navigator },
 CalendarView3117121: { screen: CalendarView3117121Navigator },
 
