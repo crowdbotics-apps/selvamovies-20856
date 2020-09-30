f6d7fe8fdf94fad3d494f051d8b7a47140c20c51
Crowdbotics <team@crowdbotics.com>
2020-09-30 00:10:57 +0000

    add package blueprint-react-native-settings-screen

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -1,5 +1,6 @@
 export const installed_blueprints = [
   //@BlueprintInsertion
+{ name: 'Settings2117122', human_name: 'Settings 2', access_route: 'Settings2117122'},
 { name: 'CalendarView3117121', human_name: 'Calendar View 3', access_route: 'CalendarView3117121', icon: 'calendar'},
 
   // you can add more installed blueprints here
--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -5,6 +5,7 @@ import {createDrawerNavigator} from 'react-navigation-drawer';
 import SplashScreen from "../features/SplashScreen";
 import SideMenu from './sideMenu';
 //@BlueprintImportInsertion
+import Settings2117122Navigator from '../features/Settings2117122/navigator';
 import CalendarView3117121Navigator from '../features/CalendarView3117121/navigator';
 
 /**
@@ -14,6 +15,7 @@ import CalendarView3117121Navigator from '../features/CalendarView3117121/naviga
 const AppNavigator = {
 
     //@BlueprintNavigationInsertion
+Settings2117122: { screen: Settings2117122Navigator },
 CalendarView3117121: { screen: CalendarView3117121Navigator },
 
     /** new navigators can be added here */
